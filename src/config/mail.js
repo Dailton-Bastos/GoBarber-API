f4fbdbbb36cdc438c8031a92beef15a171b42a21
Dailton Bastos <dailtonbastos@gmail.com>
2020-08-21 22:45:22 -0300

    feat: nodemailer config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '07fdbfc0abcbe9',
+    pass: '268d968b66a264',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreplay@gobarber.com>',
+  },
+};
