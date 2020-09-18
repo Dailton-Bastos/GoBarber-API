95580451ed189e4f5429e2284c8d636bbbd3eedf
Dailton Bastos <dailtonbastos@gmail.com>
2020-09-17 22:34:26 -0300

    fix: complements api

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,7 +26,7 @@ class User extends Model {
   }
 
   static associate(models) {
-    User.belongsTo(models.File, {
+    this.belongsTo(models.File, {
       targetKey: 'id',
       foreignKey: 'avatar_id',
       as: 'avatar',
